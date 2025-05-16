19c51b793d4cd901694e0888ae4b6e2e5822e812
Mikhail Olferiev <olferievm@gmail.com>
2025-05-15 23:44:31 -0400

    Add interferon and glucocorticoid signature

---
+++ calculations/glucocorticoid_genes.R
@@ -0,0 +1,70 @@
+#' Calculate interferon score from expression matrix or edgeR object
+#'
+#'
+#'
+#'
+#'
+#' @param x A matrix of CPM values with gene names as rownames, or an edgeR::DGEList object.
+#' @param controls A vector of column indices (or logicals) identifying control samples.
+#'
+#' @return A numeric vector of interferon scores, one per sample.
+#' @import edgeR
+#' @export
+
+
+glucocorticoid_genes <- function(x, controls = 1) {
+  requireNamespace("edgeR", quietly = TRUE)
+  
+  # Define the 21 interferon-stimulated genes
+  gluc_genes <- c( "ECHDC3", "ZBTB16", "DUSP1", "FAR2",
+                  "HDAC4", "GRB10", "ERLIN1",
+                  "RNASE1", "ATP8B4", "FKBP5",
+                  "FZD5", "TEX2", "CAMKK2",
+                  "DAAM2", "B4GALT5", "SPTLC2"
+                  )
+  
+  if (inherits(x, "DGEList")) {
+    if (!"gene_name" %in% colnames(x$genes)) {
+      stop("DGEList must have a 'gene_name' column in x$genes.")
+    }
+    
+    matched_genes <- intersect(x$genes$gene_name, gluc_genes)
+    
+    if (length(matched_genes) == 0) stop("No interferon genes found. Abort.")
+    if (length(matched_genes) < 4) stop("Only ", length(matched_genes), " genes found. Abort.")
+    if (length(matched_genes) < 8) warning("NOTE: Found ", length(matched_genes), " genes.")
+    if (anyDuplicated(matched_genes)) stop("Duplicated gene names found. Abort.")
+    
+    gene_indices <- which(x$genes$gene_name %in% matched_genes)
+    gene_labels <- x$genes$gene_name[gene_indices]
+    
+    cpm_mat <- edgeR::cpm(x, normalized.lib.sizes = TRUE,
+                          log = TRUE,
+                          prior.count = 2)
+    
+    cpm_mat <- cpm_mat[gene_indices, , drop = FALSE]
+    rownames(cpm_mat) <- gene_labels
+    
+  } else if (is.matrix(x)) {
+    matched_genes <- intersect(rownames(x), interferon_genes)
+    
+    if (length(matched_genes) == 0) stop("No interferon genes found. Abort.")
+    if (length(matched_genes) < 4) stop("Only ", length(matched_genes), " genes found. Abort.")
+    if (length(matched_genes) < 8) warning("NOTE: Found ", length(matched_genes), " genes.")
+    
+    cpm_mat <- x[matched_genes, , drop = FALSE]
+  } else {
+    stop("Unsupported input type. Provide either a CPM matrix or a DGEList object.")
+  }
+  
+  # Calculate control statistics
+  control_means <- rowMeans(cpm_mat[, controls, drop = FALSE])
+  control_sds   <- apply(cpm_mat[, controls, drop = FALSE], 1, sd)
+  
+  # Standardize expression: (x - mean) / sd
+  z_scores <- sweep(cpm_mat, 1, control_means, "-")
+  z_scores <- sweep(z_scores, 1, control_sds, "/")
+  
+  # Interferon score is the average Z-score per sample
+  colMeans(z_scores, na.rm = TRUE)
+}
