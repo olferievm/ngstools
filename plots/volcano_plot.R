791f2f4515768fad830c111ad5734bd5141c9bdb
Mikhail Olferiev <olferievm@gmail.com>
2025-09-22 15:18:47 -0400

    Volcano plot can now plot FDR and PValue, depends on user preference

--- plots/volcano_plot.R
+++ plots/volcano_plot.R
@@ -1,10 +1,10 @@
 #' Volcano Plot for Gene Expression Results
 #'
 #' This function generates a volcano plot from a `TopTags` object (from edgeR)
-#' or a data frame containing gene expression results with `logFC`, `FDR`, and
+#' or a data frame containing gene expression results with `logFC`, `FDR` (`adj.P.Value`, `adj.pval`), `PValue` (`P.Value`,`pval`) and
 #' a gene label column.
 #'
-#' @param top A `TopTags` object or a data frame with columns `logFC`, `FDR`, and `label_column`.
+#' @param top A `TopTags` object or a data frame with columns `logFC`, `FDR` (`adj.P.Value`, `adj.pval`, `PValue`, `P.Value`, `pval`) and `label_column`.
 #' @param logFC Numeric. Threshold for absolute log2 fold change. Default: 1.
 #' @param pval Numeric. FDR cutoff for significance (between 0 and 1). Default: 0.05.
 #' @param label Logical. Whether to label significant genes. Default: TRUE.
@@ -12,6 +12,8 @@
 #' @param xlim "auto" or numeric vector of length 2 for x-axis limits. Default: "auto".
 #' @param ylim "auto" or numeric vector of length 2 for y-axis limits. Default: "auto".
 #' @param max.overlaps Integer. Max overlapping labels (ggrepel). Default: 10.
+#' @param pval_colname Character. Column name to use for significance. Default: "FDR".
+#' @param logFC_colname Character. Column name to use for logFC. Default: "logFC".
 #' @param label_column Character. Column name to use for text labels. Default: "gene_name".
 #' @param color_scale Character vector of length 2. Colors for c("Not Signif.", significant_label). Default: c("black", "orange").
 #' @param label_size Numeric. Text size for gene labels. Default: 3.
@@ -30,9 +32,12 @@ volcano_plot <- function(top,
                          xlim = "auto",
                          ylim = "auto",
                          max.overlaps = 10,
+                         pval_colname = 'FDR',
+                         logFC_colname = 'logFC',
                          label_column = "gene_name",
                          color_scale = c("black", "orange"),
-                         label_size = 3) {
+                         label_size = 3,
+                         max_points = FALSE) {
   
   # Load required packages
   if (!requireNamespace("ggplot2", quietly = TRUE)) stop("Package 'ggplot2' is required.")
@@ -47,14 +52,25 @@ volcano_plot <- function(top,
   if (!is.data.frame(top)) stop("'top' must be a data.frame or a TopTags object.")
   
   # Check required columns
-  required_cols <- c("logFC", "FDR", label_column)
+  required_cols <- c(logFC_colname, pval_colname, label_column)
   missing_cols <- setdiff(required_cols, colnames(top))
+  
   if (length(missing_cols) > 0)
-    stop("Missing column(s): ", paste(missing_cols, collapse = ", "))
+    stop("Missing column(s): ", paste(missing_cols, collapse = ", "), "\n - Check the input or adjust: pval_column, logFC_column, label_column")
+  
+  top <- top %>% dplyr::select(all_of(c(logFC_colname, pval_colname, label_column)))
+  
+  if(logFC_colname != 'logFC'){
+    top <- top %>% dplyr::rename(FDR = !!sym(logFC_colname))
+  }
+  if(pval_colname != 'FDR'){
+    top <- top %>% dplyr::rename(FDR = !!sym(pval_colname))
+  }
+  
   
   # Check input validity
   if (!(logFC > 0)) stop("logFC must be > 0.")
-  if (pval <= 0 || pval >= 1) stop("pval must be between 0 and 1.")
+  if (pval <= 0 || pval >= 1) stop("pval cut-off must be between 0 and 1.")
   if (!is.character(color_scale) || length(color_scale) != 2)
     stop("color_scale must be a character vector of length 2.")
   
@@ -69,7 +85,15 @@ volcano_plot <- function(top,
   }
   
   # Define dynamic significance label
-  signif_label <- sprintf("FDR < %.2g", pval)
+  signif_label = case_when(
+    pval_colname == 'FDR' ~ sprintf("FDR < %.2g", pval),
+    pval_colname == 'PValue' ~ sprintf("pval < %.2g", pval),
+    pval_colname == 'P.Value' ~ sprintf("pval < %.2g", pval),
+    pval_colname == 'pval' ~ sprintf("pval < %.2g", pval),
+    pval_colname == 'adj.p.value' ~ sprintf("adj.pval < %.2g", pval),
+    pval_colname == 'adj.pval' ~ sprintf("adj.pval < %.2g", pval),
+    pval_colname == 'apval' ~ sprintf("adj.pval < %.2g", pval),
+  )
   
   # Annotate data with significance
   top$Threshold <- ifelse(abs(top$logFC) > logFC & top$FDR < pval,
@@ -78,6 +102,14 @@ volcano_plot <- function(top,
   # Subset significant points for labeling
   topnames <- dplyr::filter(top, Threshold == signif_label)
   
+  # Plot size reduction. Reduced unlabeled points.
+  if(max_points > 0){
+    if(max_points < nrow(top)){
+       top$pick <- FALSE
+       top$pick[sample(1:nrow(top), max_points)] <- TRUE
+       top <- top %>% dplyr::filter(Threshold == signif_label | pick)
+    }
+  }
   # Create plot
   g <- ggplot(top, aes(x = logFC, y = -log10(FDR), color = Threshold)) +
     geom_point(alpha = 0.5, size = 1.75) +
